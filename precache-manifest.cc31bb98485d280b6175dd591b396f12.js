ca79252ad49ca8842b58deca65cedf58542cc8d2
Misplaced01 <00232918@uca.edu.sv>
2019-11-05 16:07:08 -0600

    Updates

---
+++ precache-manifest.cc31bb98485d280b6175dd591b396f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0a6ea391e0a89ca9c87d308f5c2f5c",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "ca54791b14260cbb8ed0",
+    "url": "/tictac/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "0ed91b759889665edf14",
+    "url": "/tictac/static/js/2.94380357.chunk.js"
+  },
+  {
+    "revision": "ca54791b14260cbb8ed0",
+    "url": "/tictac/static/js/main.c6bbbed9.chunk.js"
+  },
+  {
+    "revision": "e19c5e22db88a8b68a0b",
+    "url": "/tictac/static/js/runtime-main.a646fcd2.js"
+  }
+]);
